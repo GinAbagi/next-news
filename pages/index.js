9d0ca38d9046bcc78f7af4f9ee2adcf2bac640ce
ginabagi <415308@student.fontys.nl>
2021-01-13 22:17:24 +0100

    Navigation update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { Toolbar } from '../components/toolbar';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return(<>Hello world</>);
+  return(
+    <div className='page-container'>
+      <Toolbar />
+      <div className={styles.main}>
+        <h1>Next.js News App</h1>
+
+        <h3>You are at the right places for the latest news articles
+        </h3>
+      </div>
+    </div>
+ );
 }
